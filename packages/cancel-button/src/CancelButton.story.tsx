d5e828b7d6d4c83b705803a8aa380e7ed0d10bbf
Adam Thompson <adam.thompson@mongodb.com>
2023-03-29 15:54:04 -0400

    Update CancelButton.story.tsx

--- packages/cancel-button/src/CancelButton.story.tsx
+++ packages/cancel-button/src/CancelButton.story.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory } from '@storybook/react';
 
+import ConfirmationModal from '@leafygreen-ui/confirmation-modal';
 import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
 import { StoryMeta } from '@leafygreen-ui/lib';
+import { palette } from '@leafygreen-ui/palette';
 import { Body } from '@leafygreen-ui/typography';
 
 import { CancelButton } from '.';
@@ -25,20 +27,35 @@ const Template: ComponentStory<typeof CancelButton> = ({
   // eslint-disable-next-line react/prop-types
   darkMode,
   ...props
-}) => (
-  <LeafyGreenProvider darkMode={darkMode}>
-    <Body darkMode={darkMode}>
-      Click the button to cancel your subscription
-    </Body>
-    <CancelButton
-      darkMode={darkMode}
-      {...props}
-      container={
-        document.querySelector('#story--components-cancelbutton--basic') ||
-        document.querySelector('#root')
-      }
-    />
-  </LeafyGreenProvider>
-);
+}) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <LeafyGreenProvider darkMode={darkMode}>
+      <Body darkMode={darkMode}>
+        Click the button to cancel your subscription
+      </Body>
+      <CancelButton
+        darkMode={darkMode}
+        {...props}
+        onClick={() => setModalOpen(true)}
+        container={
+          document.querySelector('#story--components-cancelbutton--basic') ||
+          document.querySelector('#root')
+        }
+      />
+      <ConfirmationModal
+        open={modalOpen}
+        title="Ok to cancel?"
+        buttonText="Ok"
+        onCancel={() => setModalOpen(false)}
+        onConfirm={() => setModalOpen(false)}
+        closeIconColor={palette.red.base}
+      >
+        {' '}
+      </ConfirmationModal>
+    </LeafyGreenProvider>
+  );
+};
 
 export const Basic = Template.bind({});
